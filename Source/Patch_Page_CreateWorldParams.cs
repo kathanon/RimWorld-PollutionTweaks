43eb59356b77bd6e52d3ece969e48641a2c199e4
kathanon <cower@cower.se>
2023-05-18 00:02:18 +0200

    Improved compatibility with other mods.

--- Source/Patch_Page_CreateWorldParams.cs
+++ Source/Patch_Page_CreateWorldParams.cs
@@ -3,6 +3,8 @@ using RimWorld;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -31,11 +33,22 @@ public static class Patch_Page_CreateWorldParams {
         range.max = VanillaMax;
     }
 
-    [HarmonyPostfix]
+    [HarmonyTranspiler]
     [HarmonyPatch(typeof(Page_CreateWorldParams), nameof(Page_CreateWorldParams.DoWindowContents))]
-    public static void DoWindowContents(Rect rect) {
-        if (!ModsConfig.BiotechActive) return;
-        Rect area = new Rect(rect.x, rect.y + YSkip, (rect.width - Margin) * 0.5f, Height);
+    public static IEnumerable<CodeInstruction> DoWindowContents_Transpiler(IEnumerable<CodeInstruction> orig) {
+        var pollution = AccessTools.Field(typeof(Page_CreateWorldParams), "pollution");
+        foreach (var instr in orig) {
+            yield return instr;
+            if (instr.opcode == OpCodes.Stfld && instr.operand is FieldInfo arg && arg == pollution) {
+                yield return new CodeInstruction(OpCodes.Ldarg_1);
+                yield return new CodeInstruction(OpCodes.Ldloca_S, 7);
+                yield return CodeInstruction.Call(typeof(Patch_Page_CreateWorldParams), nameof(WindowContentAdditions));
+            }
+        }
+    }
+
+    public static void WindowContentAdditions(Rect rect, ref float curY) {
+        Rect area = new Rect(rect.x, curY, (rect.width - Margin) * 0.5f, Height);
         TooltipHandler.TipRegion(area, Strings.PollutionTip);
         area.y += YStep;
 
@@ -49,6 +62,8 @@ public static class Patch_Page_CreateWorldParams {
         TooltipHandler.TipRegion(area, Strings.PollutionSpreadTip);
         Widgets.Label(area.LeftPartPixels(LabelWidth), Strings.PollutionSpread);
         Widgets.Checkbox(new(area.x + LabelWidth, area.y), ref adjust);
+
+        curY = area.y;
     }
 
     [HarmonyPostfix]
